cef032d5927678fe1bea5485170ace4f1bd0367a
064678147 <richardmc.song@mail.utoronto.ca>
2015-10-28 20:00:27 -0400

    push to desktop

---
+++ client/js/mentorship.js
@@ -0,0 +1,15 @@
+if (Meteor.isClient) {
+  Template.mentorship.helpers({
+    'getPosts': function() {
+      return Posts.find().fetch();
+    }
+  });
+
+  Template.mentorship.onRendered(function() {
+    this.$('.mas').masonry({
+      itemSelector: ".mas-item",
+      percentPosition: true,
+      gutter: 0
+    });
+  })
+};
--- server/backend.js
+++ server/backend.js
@@ -1,3 +1,5 @@
+Posts = new Mongo.Collection("Posts");
+
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
